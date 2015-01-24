f4357fd7f4ee7fb8cd1fb9907c593d17df33c985
perelin <patino@p2lab.de>
2015-01-24 14:45:30 +0100

    stuuf

--- partyhard/game_view.py
+++ partyhard/game_view.py
@@ -1,8 +1,13 @@
+'''
+Fonts: http://www.nerdparadise.com/tech/python/pygame/basics/part5/
+'''
+
 import pygame
 from item_sprites import *
 from item_models import *
 from event_manager import *
 from random import randint
+from game_sound import *
 
 WIDTH = 1024
 HEIGHT = 680
@@ -24,15 +29,30 @@ class GameView(object):
         self.background.fill((0,255,0))
         self.screen.blit(self.background,(0,0))
 
-        self.demosprite = ItemSprite()
+        #self.demosprite = ItemSprite()
 
         self.itemgroup = pygame.sprite.Group()
-        self.itemgroup.add(self.demosprite)        
-        self.itemgroup.draw(self.screen)
+        #self.itemgroup.add(self.demosprite)        
+        #self.itemgroup.draw(self.screen)
+
+        self.textgroup = pygame.sprite.Group()
 
         #self.screen.blit(self.demosprite.image, DEMOSPRITE_POSITION)
 
+        font = pygame.font.SysFont(None, 72)
+        text = font.render("Party Hard!", True, (0, 128, 0))
+        #text_position = text.get_rect()
+
+        self.textgroup.add(text)
+
+        self.screen.blit(text,(WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+
         pygame.display.flip()
+
+        self.game_sound = GameSound()
+
+        main_track = self.game_sound.load_main_track()
+        #main_track.play(-1)
 
     #---------------------------------------------------------------------- 
 
@@ -43,10 +63,16 @@ class GameView(object):
             # falling items
             for item in self.itemgroup:
                 item.rect.y += item.fall_speed
+                # items falling out of the screen
+                if item.rect.y > HEIGHT:
+                    item.kill()
+                    print "item killed, current items in group: " + str(len(self.itemgroup)) 
 
             #print self.screen
             self.itemgroup.clear(self.screen,self.background)
             self.itemgroup.draw(self.screen)
+
+
             pygame.display.flip()
 
         elif isinstance(event,SpawnItemEvent):
